dda9f799fec4ff3585a761a48a889463dcd1c69f
IcaroRaphael <icaro2551@gmail.com>
2025-04-17 13:15:11 -0300

    Seção 01 (Projeto Console - Análise de código)

---
+++ Seção 01 (Introdução)/ProjetoNet8/ProjetoNet8/Program.cs
@@ -0,0 +1,9 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Usando as instruções");
+        Console.WriteLine("De nível Superior");
+    }
+}
