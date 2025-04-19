ad057259a53aa291519413c0ddcea07d984966c4
IcaroRaphael <icaro2551@gmail.com>
2025-04-19 19:33:32 -0300

    Seção 02 (Tipos de referência)

---
+++ Seção 02 (Fundamentos)/CSFundamentos1/TipoDados4/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Definindo variáveis do tipo string e object e dynamic");
+Console.WriteLine();
+
+string nome = "Curso C#";
+String titulo = "Curso C# Essencial";
+
+Console.WriteLine(nome);
+Console.WriteLine(titulo);
+
+string valor = "Isto é uma string";
+valor = "Isto é uma string alterada";
+valor = "teste";
+
+dynamic nota = 10;
+dynamic valor1 = 8.55m;
+dynamic nome1 = "Maria";
+dynamic ativa = true;
+dynamic letra = 'A';
+
+Console.WriteLine(nota);
+Console.WriteLine(valor1);
+Console.WriteLine(nome1);
+Console.WriteLine(ativa);
+Console.WriteLine(letra);
+
+Console.ReadLine();
