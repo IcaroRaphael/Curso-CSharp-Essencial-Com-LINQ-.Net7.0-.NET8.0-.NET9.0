fb66b6cd3dbdb62661dd45f69f4e7f1d11a1e069
IcaroRaphael <icaro2551@gmail.com>
2025-04-20 11:10:15 -0300

    Seção 02 (Nullable Types)

---
+++ Seção 02 (Fundamentos)/CSFundamentos1/NullableTypes1/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("## Nullable Types ##\n");
+
+int? b = null;
+if (b.HasValue)
+{
+    Console.WriteLine($"b = {b}");
+}
+else
+{
+    Console.WriteLine("b não possui um valor (null)");
+}
+
+Console.ReadKey();
