613a9ba00f59bc24b610cd19974df81805f30273
IcaroRaphael <icaro2551@gmail.com>
2025-04-22 08:34:46 -0300

    Seção 02 (Inferência de tipos)

---
+++ Seção 02 (Fundamentos)/CSFundamentos1/InferenciaTipos1/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("## Inferência Tipos ##\n");
+
+var idade = 25;
+var nome = "Maria";
+var salario = 2500.00m;
+
+Console.WriteLine($"{nome} tem {idade} e ganha {salario:F2}");
+
+Console.ReadKey();
