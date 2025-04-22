3a9d2db832469a444299474e1922102d843153c3
IcaroRaphael <icaro2551@gmail.com>
2025-04-22 11:38:27 -0300

    Seção 02 (Operadores de atribuição)

--- Seção 02 (Fundamentos)/CSFundamentos1/InferenciaTipos1/Program.cs
+++ Seção 02 (Fundamentos)/CSFundamentos1/InferenciaTipos1/Program.cs
@@ -6,4 +6,26 @@ var salario = 2500.00m;
 
 Console.WriteLine($"{nome} tem {idade} e ganha {salario:F2}");
 
+
+////Var limitações
+//var salario = null;
+//var titulo;
+//var salario, imposto, total;
+
+////Não posso mudar o tipo após inicializar
+//var num = 10;
+//num = num + 20;
+//num = "Teste"; //Não pode mudar o tipo
+
+var teste = new Teste();
+teste.MeuMetodo();
+
 Console.ReadKey();
+
+class Teste
+{
+    public void MeuMetodo()
+    {
+        Console.WriteLine("Meu método");
+    }
+}
