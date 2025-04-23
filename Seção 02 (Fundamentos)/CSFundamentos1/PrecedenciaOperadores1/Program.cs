7a97b3565a9559d04fb798494ffdf91e6cb26839
IcaroRaphael <icaro2551@gmail.com>
2025-04-22 21:45:25 -0300

    Seção 02 (Precedência e associatividade de operadores)

---
+++ Seção 02 (Fundamentos)/CSFundamentos1/PrecedenciaOperadores1/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("## Precedência de operadores ##\n");
+
+//int x = (10 - 2) * 3;
+//Console.WriteLine(x); //Resultado: 24
+//int y = 10 - 2 * 3;
+//Console.WriteLine(y); //Resultado: 4
+
+//bool a = !(9 != 8) && 5 >= 7 || 8 >= 6;
+//Console.WriteLine(a);
+//bool b = !(9 != 8) && (5 >= 7 || 8 >= 6);
+//Console.WriteLine(b);
+
+int a = 5, b = 6, c = 4;
+int r = --a * b - ++c;
+Console.WriteLine(r);
+
+Console.ReadKey();
