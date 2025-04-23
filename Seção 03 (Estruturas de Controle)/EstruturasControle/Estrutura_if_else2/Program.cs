d9833c58150086821fd80edff9687ec77d306dbd
IcaroRaphael <icaro2551@gmail.com>
2025-04-23 18:40:00 -0300

    Seção 03 (Instrução if-else)

---
+++ Seção 03 (Estruturas de Controle)/EstruturasControle/Estrutura_if_else2/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("## Instrução if-else ##\n");
+
+// Console.Write("Informe a nota do aluno: ");
+// var nota = Convert.ToInt32(Console.ReadLine());
+// if(nota > 5){
+//     Console.WriteLine("Aluno Aprovado");
+// }
+// else{
+//     Console.WriteLine("Aluno Reprovado");
+// }
+
+Console.Write("Informe o valor de x: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Informe o valor de y: ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+if(x > y){
+    Console.WriteLine("x é maior que y");
+}
+else{
+    if(x < y){
+        Console.WriteLine("x é menor que y");
+    }
+    else{
+        Console.WriteLine("x é igual a y");
+    }
+}
+
+Console.WriteLine("Fim do processamento");
+Console.ReadKey();
