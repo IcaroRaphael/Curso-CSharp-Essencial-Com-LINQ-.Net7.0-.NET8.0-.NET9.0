a96c096dac5ab72b638bac6ad21b96d916b6fc17
IcaroRaphael <icaro2551@gmail.com>
2025-04-22 21:57:50 -0300

    Seção 02 (Nullable Reference Types)

---
+++ Seção 02 (Fundamentos)/CSFundamentos1/TiposReferenciaAnulaveis1/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("## Tipos de Referência Anuláveis ##\n");
+
+//string? nome = null;
+string nome = "";
+
+Console.WriteLine(nome);
+Console.WriteLine(nome?.ToUpper());
+
+Console.ReadKey();
