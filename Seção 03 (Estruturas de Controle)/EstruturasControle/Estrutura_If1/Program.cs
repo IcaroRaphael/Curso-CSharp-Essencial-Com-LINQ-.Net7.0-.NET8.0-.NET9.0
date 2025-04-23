1556600d75479d18d148a6d1d74ff0e609731df6
IcaroRaphael <icaro2551@gmail.com>
2025-04-23 18:18:46 -0300

    Seção 03 (Instrução if)

---
+++ Seção 03 (Estruturas de Controle)/EstruturasControle/Estrutura_If1/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("## Instrução if ##");
+
+Console.Write("Informe o valor de x: ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+System.Console.Write("Informe o valor de y: ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+if(x > y){
+    Console.WriteLine("x é maior que y");
+}
+if(x < y){
+    Console.WriteLine("x é menor que y");
+}
+if(x == y){
+    Console.WriteLine("x é igual a y");
+}
+
+Console.ReadKey();
