77d6f28ff92b92dd315fed96276ba13ca2de7556
IcaroRaphael <icaro2551@gmail.com>
2025-04-23 21:39:27 -0300

    Seção 03 (Instrucao if-else-if)

---
+++ Seção 03 (Estruturas de Controle)/EstruturasControle/Estrutura_if_else_if3/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("## Instrução if-else-if ##\n");
+
+Console.Write("Informe a nota do aluno: ");
+var nota = Convert.ToDouble(Console.ReadLine());
+
+if(nota < 5)
+{
+    Console.WriteLine("ALUNO REPROVADO");
+}
+else if(nota >= 5 && nota < 6)
+{
+    Console.WriteLine("ALUNO EM RECUPERAÇÃO");
+}
+else if (nota >= 6 && nota <= 9)
+{
+    Console.WriteLine("ALUNO APROVADO");
+}
+else
+{
+    Console.WriteLine("ALUNO APROVADO COM DISTINÇÃO");
+}
+
+Console.WriteLine("Fim do processamento");
+
+Console.ReadKey();
