71a3b09dcc2c80be36eb7b19b0e8dee972161ca3
IcaroRaphael <icaro2551@gmail.com>
2025-04-24 10:21:30 -0300

    Seção 03 (Instrução while I)

---
+++ Seção 03 (Estruturas de Controle)/EstruturasControle/Repeticao_while/Program.cs
@@ -0,0 +1,39 @@
+﻿Console.WriteLine("## Instrução While ##\n");
+
+// var i = 1;
+// while(i <= 10)
+// {
+//     Console.WriteLine($"i = {i}");
+//     i++;
+// }
+
+
+// var i = 10;
+// while(i > 0)
+// {
+//     Console.WriteLine($"i = {i}");
+//     i--;
+// }
+
+int numero;
+int contador = 1;
+
+Console.Write("Digite um numero maior que zero: ");
+numero = Convert.ToInt32(Console.ReadLine());
+
+if (numero > 0)
+{
+    Console.WriteLine($"## Tabuada do {numero}\n");
+    while (contador <= 10)
+    {
+        Console.WriteLine($"{numero} x {contador} = {numero * contador}");
+        contador++;
+    }
+}
+else
+{
+    Console.WriteLine("Número deve ser maior que zero!!");
+}
+
+Console.WriteLine("Fim do processamento...");
+Console.ReadKey();
