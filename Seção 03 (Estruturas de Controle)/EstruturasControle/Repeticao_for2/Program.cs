179260e07a386db271742f5dda06647e44968c6d
IcaroRaphael <icaro2551@gmail.com>
2025-04-25 09:56:19 -0300

    Seção 03 (Instrução for II)

---
+++ Seção 03 (Estruturas de Controle)/EstruturasControle/Repeticao_for2/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("## Loop For\n");
+
+// for (int i = 0, j = 0; i+j <= 20; i++, j++)
+// {
+//     Console.WriteLine($"i = {i} e j = {j}");
+// }
+
+// int i = 1;
+// for( ; i <= 10; )
+// {
+//     Console.WriteLine($"i = {i}");
+//     i++;
+// }
+
+for(int i = 0; i < 5; i++)
+{
+    for(int j = 0; j < 5; j++)
+    {
+        Console.Write($"({i}, {j}) ");
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine("Fim do processamento...");
+Console.ReadKey();
