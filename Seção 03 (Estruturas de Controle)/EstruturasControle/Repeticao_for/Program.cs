539eadb0ddb39f1da74b18b5a6a6f4012da05ac5
IcaroRaphael <icaro2551@gmail.com>
2025-04-25 08:28:55 -0300

    Seção 03 (Instrução for I)

---
+++ Seção 03 (Estruturas de Controle)/EstruturasControle/Repeticao_for/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("## Instrução for ##\n");
+
+int resultado, numero;
+
+Console.Write("Informe um numero inteiro maior que 0: ");
+numero = Convert.ToInt32(Console.ReadLine());
+
+if(numero > 0  )
+{
+    for(int i = 1; i <= 10; i++){
+    resultado = numero * i;
+    // Console.WriteLine(numero + " X " + i + " = " + resultado);
+    Console.WriteLine($"{numero} x {i} = {resultado}");
+    }
+}
+else
+{
+    Console.WriteLine("Número inválido");
+}
+
+Console.WriteLine("\nFim do processamento...");
+Console.ReadKey();
