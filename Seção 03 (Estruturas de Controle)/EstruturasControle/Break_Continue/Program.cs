540d042a59569dfc090ef2dacb1857a645474401
IcaroRaphael <icaro2551@gmail.com>
2025-04-27 20:01:43 -0300

    Seção 03 (Instruções break e continue)

---
+++ Seção 03 (Estruturas de Controle)/EstruturasControle/Break_Continue/Program.cs
@@ -0,0 +1,42 @@
+﻿Console.WriteLine("## Break e Continue");
+
+//for(; ; )
+//{
+//    Console.Write("Tecle algo (X sai): ");
+//    var opcao = Console.ReadLine();
+//    Console.WriteLine(opcao.ToUpper());
+//    if(opcao == "X" || opcao == "x")
+//    {
+//        break;
+//    }
+//}
+
+//for(int i = 1; i <= 10; i++)
+//{
+//    if(i == 4)
+//    {
+//        continue;
+//    }
+//    Console.WriteLine(i);
+//}
+
+int n = 1;
+while (n <= 10)
+{
+    if (n == 5)
+    {
+        n++;
+        continue;
+    }
+
+    if (n > 8)
+    {
+        break;
+    }
+
+    Console.WriteLine(n);
+    n++;
+}
+
+Console.WriteLine("Fim do processamento...");
+Console.ReadKey();
