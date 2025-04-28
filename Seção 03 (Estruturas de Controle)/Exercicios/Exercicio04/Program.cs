7ad8056e5910656ab6422e3e0a69cc4343d22b7f
IcaroRaphael <icaro2551@gmail.com>
2025-04-28 18:13:18 -0300

    Seção 03 (Exercicio04 - Resolvido)

---
+++ Seção 03 (Estruturas de Controle)/Exercicios/Exercicio04/Program.cs
@@ -0,0 +1,26 @@
+﻿while (true)
+{
+    Console.Write("\nInsira um número natural (0 para sair): ");
+    int numero = int.Parse(Console.ReadLine());
+
+    if (numero == 0)
+    {
+        Console.WriteLine("Finalizando programa...");
+        break;
+    }
+
+    if (numero < 0)
+    {
+        Console.WriteLine("Número inválido. Tente novamente.");
+        continue;
+    }
+    else
+    {
+        for(int i = 1; i <= 10; i++)
+        {
+            Console.WriteLine($"{numero} x {i} = {numero*i}");
+        }
+        Console.ReadKey();
+        Console.Clear();
+    }
+}
