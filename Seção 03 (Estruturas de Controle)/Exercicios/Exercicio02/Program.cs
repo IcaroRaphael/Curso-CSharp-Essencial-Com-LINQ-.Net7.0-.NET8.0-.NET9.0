800fee79dd345c08b6ed3bb10e09e73dcdb931ec
IcaroRaphael <icaro2551@gmail.com>
2025-04-28 17:49:28 -0300

    Seção 03 (Exercicio02 - Resolvido)

---
+++ Seção 03 (Estruturas de Controle)/Exercicios/Exercicio02/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.WriteLine("Cálculo da equação do segundo grau (ax^2+bx+c) :");
+
+Console.Write("Informe o valor de A: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Informe o valor de B: ");
+int b = int.Parse(Console.ReadLine());
+Console.Write("Informe o valor de C: ");
+int c = int.Parse(Console.ReadLine());
+
+int delta = (b * b) - (4 * a * c);
+
+if(delta > 0)
+{
+    int x1 = (int)((- b + Math.Sqrt(delta)) / (2 * a));
+    int x2 = (int)((- b - Math.Sqrt(delta)) / (2 * a));
+    Console.WriteLine("Ambas as raízes são reais e diferentes");
+    Console.WriteLine($"Primeira raiz x1 = {x1}");
+    Console.WriteLine($"Segunda raiz x2 = {x2}");
+}
+else if(delta == 0)
+{
+    int x = (int)( -b / (2 * a));
+    Console.WriteLine("Só há uma raiz.");
+    Console.WriteLine($"A raiz é: {x}");
+}
+else
+{
+    Console.WriteLine("As raízes são imaginárias;");
+    Console.WriteLine("Sem solução para os números reais.");
+}
+Console.ReadKey();
