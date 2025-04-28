8d81ebd57013483fd63598a6fec31cccfab9fea1
IcaroRaphael <icaro2551@gmail.com>
2025-04-28 08:45:26 -0300

    Seção 03 (Exercicio01 - Resolvido)

---
+++ Seção 03 (Estruturas de Controle)/Exercicios/Exercicio01/Program.cs
@@ -0,0 +1,20 @@
+﻿int maiorNumero = 0, posicaoMaior = 0;
+for(int i = 1; i <= 3; i++)
+{
+    Console.Write($"Insira o {i}º número: ");
+    int numero = Convert.ToInt32( Console.ReadLine());
+
+    if(i == 1)
+    {
+        maiorNumero = numero;
+        posicaoMaior = i;
+    }
+    else if(numero > maiorNumero)
+    {
+        maiorNumero = numero;
+        posicaoMaior = i;
+    }
+}
+
+Console.WriteLine($"O {posicaoMaior}º número: {maiorNumero} é o maior");
+Console.ReadKey();
