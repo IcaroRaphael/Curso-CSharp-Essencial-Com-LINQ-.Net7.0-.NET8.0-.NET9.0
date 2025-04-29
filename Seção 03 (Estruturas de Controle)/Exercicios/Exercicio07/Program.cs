01f37d22ceccf889767a031dbd5b6e600db878b3
IcaroRaphael <icaro2551@gmail.com>
2025-04-28 22:29:59 -0300

    Seção 03 (Exercicio07 - Resolvido)

---
+++ Seção 03 (Estruturas de Controle)/Exercicios/Exercicio07/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Insira o tamanho di triângulo:");
+int altura = int.Parse(Console.ReadLine());
+
+Console.WriteLine();
+for (int i = 1; i <= altura; i++)
+{
+    for (int j = 0; j < i; j++)
+    {
+        Console.Write("*");
+    }
+    Console.WriteLine();
+}
+Console.ReadKey();
