a3646c911843c8d52660b5b77d89e6a66f6cc0fa
IcaroRaphael <icaro2551@gmail.com>
2025-04-28 22:09:55 -0300

    Seção 03 (Exercicio05 - Resolvido)

---
+++ Seção 03 (Estruturas de Controle)/Exercicios/Exercicio05/Program.cs
@@ -0,0 +1,40 @@
+﻿while (true)
+{
+    Console.Clear();
+    Console.WriteLine("Qual a instrução para sair de um loop?");
+    Console.WriteLine("a. quit");
+    Console.WriteLine("b. continue");
+    Console.WriteLine("c. break");
+    Console.WriteLine("d. exit");
+    Console.Write("Qual a opção correta (Tecle X para sair)? R:");
+    string opcao = Console.ReadLine();
+
+    switch (opcao)
+    {
+        case "a":
+            Console.WriteLine("ERRADO: Instrução 'quit' não existe.");
+            Console.ReadKey();
+            break;
+        case "b":
+            Console.WriteLine("ERRADO: Instrução 'continue' não sai do loop.");
+            Console.ReadKey();
+            break;
+        case "c":
+            Console.WriteLine("CORRETO: Instrução 'break' sai do loop.");
+            Console.ReadKey();
+            break;
+        case "d":
+            Console.WriteLine("ERRADO: Instrução 'exit' não existe.");
+            Console.ReadKey();
+            break;
+        case "X":
+        case "x":
+            Console.WriteLine("Saindo do programa...");
+            Console.ReadKey();
+            return;
+        default:
+            Console.WriteLine("Opção inválida. Tente novamente.");
+            Console.ReadKey();
+            break;
+    }
+}
