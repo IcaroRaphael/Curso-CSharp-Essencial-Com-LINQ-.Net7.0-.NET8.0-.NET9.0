8e5f7d2b96a4c020d07c08b1946d525b1a308be7
IcaroRaphael <icaro2551@gmail.com>
2025-04-29 08:12:02 -0300

    Seção 05 (Arrays I)

---
+++ Seção 05 (Array, ArrayList e List)/ArrayArraylistList/Arrays1/Program.cs
@@ -0,0 +1,24 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        //Declaração do Array
+        int[] numeros;
+        //Alocar memoria
+        numeros = new int[10] {1,2,3,4,5,6,7,8,9,10};
+        string[] nomes = {"Ana", "Maria", "Marta", "Paulo", "Carlos"};
+        Console.WriteLine(nomes[0]);
+        Console.WriteLine(nomes[1]);
+        Console.WriteLine(nomes[2]);
+        Console.WriteLine(nomes[3]);
+        Console.WriteLine(nomes[4]);
+
+
+        //Declarando um array
+        int[] numeros1;
+        //Alocar memoria
+        numeros1 = new int[3];
+
+        Console.ReadKey();
+    }
+}
