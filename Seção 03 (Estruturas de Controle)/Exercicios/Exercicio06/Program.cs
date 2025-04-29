722d3a7a2a2afe77a9d0146c82a70bbac648e0ba
IcaroRaphael <icaro2551@gmail.com>
2025-04-28 22:21:12 -0300

    Seção 03 (Exercicio06 - Resolvido)

---
+++ Seção 03 (Estruturas de Controle)/Exercicios/Exercicio06/Program.cs
@@ -0,0 +1,41 @@
+﻿// Incrementando 2 em cada passo
+for(int i = 10; i <= 20; i += 2)
+{
+    if(i != 16)
+    {
+        Console.Write($"{i} ");
+    }
+}
+Console.WriteLine();
+
+// Incrementando 1 em cada passo
+for (int i = 10; i <= 20; i++)
+{
+    if(i % 2 == 0)
+    {
+        if(i != 16)
+        {
+            Console.Write($"{i} ");
+        }
+    }
+}
+Console.WriteLine();
+
+// Com loop infinito (use "break" e "continue")
+int j = 10;
+while (true)
+{
+    if(j % 2 == 0)
+    {
+        if (j != 16)
+        {
+            Console.Write($"{j} ");
+        }
+    }
+    j++;
+    if(j > 20)
+    {
+        break;
+    }
+}
+Console.WriteLine();
