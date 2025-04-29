30b282b7d1394e9ab15a6a2dacbc9cea2025db94
IcaroRaphael <icaro2551@gmail.com>
2025-04-28 22:39:12 -0300

    Seção 03 (Exercicio08 - Resolvido)

---
+++ Seção 03 (Estruturas de Controle)/Exercicios/Exercicio08/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Insira um número: ");
+int num = int.Parse(Console.ReadLine());
+Console.WriteLine();
+
+Console.Write($"{num}! = ");
+int fatorial = 1;
+for(int i = num; i > 0; i--)
+{
+    fatorial *= i;
+    if(i == 1)
+    {
+        Console.Write($"{i}");
+    }
+    else
+    {
+        Console.Write($"{i}*");
+    }
+}
+Console.Write($" = {fatorial}\n");
+Console.ReadKey();
