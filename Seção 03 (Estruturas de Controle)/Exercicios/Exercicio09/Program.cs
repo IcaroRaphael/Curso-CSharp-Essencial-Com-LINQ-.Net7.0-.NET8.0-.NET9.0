87887f936db31bdfcadd1fef0a11146ae8f66d1f
IcaroRaphael <icaro2551@gmail.com>
2025-04-28 22:46:03 -0300

    Seção 03 (Exercicio09 - Resolvido)

---
+++ Seção 03 (Estruturas de Controle)/Exercicios/Exercicio09/Program.cs
@@ -0,0 +1,13 @@
+﻿int i = 1, j = 1;
+do
+{
+    j = 1;
+    do
+    {
+        Console.Write($"{i} x {j} = {i*j}\n");
+        j++;
+    } while (j <= 10);
+    Console.WriteLine();
+    i++;
+} while (i <= 6); ;
+Console.ReadKey();
