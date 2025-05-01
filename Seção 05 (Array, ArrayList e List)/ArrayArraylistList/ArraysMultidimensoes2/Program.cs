0533193a4288a2a1a8927c3b6688cc95cf3446b1
IcaroRaphael <icaro2551@gmail.com>
2025-05-01 19:59:55 -0300

    Seção 05 (Arrays Multidimensionais II)

---
+++ Seção 05 (Array, ArrayList e List)/ArrayArraylistList/ArraysMultidimensoes2/Program.cs
@@ -0,0 +1,24 @@
+﻿int[,] n = 
+    {
+        {11, 22, 33}, 
+        {44, 55, 66}, 
+        {77, 88, 99}
+    };
+
+Console.WriteLine();
+for(int i = 0; i < n.GetLength(0); i++)
+{
+    for(int j = 0; j < n.GetLength(1); j++)
+    {
+        Console.Write(n[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine();
+foreach(int num in n)
+{
+    Console.Write(num + " ");
+}
+
+Console.ReadKey();
