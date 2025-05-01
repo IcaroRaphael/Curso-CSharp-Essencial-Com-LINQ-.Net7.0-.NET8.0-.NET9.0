ae65a7aae9492c80a0ff534fe3e6d7e26f9dd220
IcaroRaphael <icaro2551@gmail.com>
2025-05-01 16:25:59 -0300

    Seção 05 (Exercício - Arrays)

---
+++ Seção 05 (Array, ArrayList e List)/ArrayArraylistList/ExercicioArrays/Program.cs
@@ -0,0 +1,21 @@
+﻿string[] nomes = new string[5];
+double[] notas = new double[5];
+
+double somaTotal = 0;
+for (int i = 0; i < nomes.Length; i++)
+{
+    Console.Write($"Digite o nome do {i + 1}º aluno: ");
+    nomes[i] = Console.ReadLine();
+    Console.Write($"Digite a nota do {i + 1}º aluno: ");
+    notas[i] = double.Parse(Console.ReadLine());
+    Console.WriteLine();
+}
+
+for (int i = 0; i < nomes.Length; i++)
+{
+    Console.WriteLine($"{nomes[i]}: {notas[i]:F2}");
+    somaTotal += notas[i];
+}
+
+Console.WriteLine($"\nMédia: {(somaTotal/notas.Length):F2}");
+Console.ReadKey();
