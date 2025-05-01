da99e84dbe80a9fa7c4f24f31eec47424be2bfaf
IcaroRaphael <icaro2551@gmail.com>
2025-05-01 20:30:30 -0300

    Seção 05 (Exercício - Arrays Bidimensionais)

---
+++ Seção 05 (Array, ArrayList e List)/ArrayArraylistList/ExArraysBidimensoes/Program.cs
@@ -0,0 +1,23 @@
+﻿string[,] nomes = new string[2, 5];
+
+Console.WriteLine("INSIRA OS NOMES");
+for(int i = 0; i < nomes.GetLength(0); i++)
+{
+    for(int j = 0; j < nomes.GetLength(1); j++)
+    {
+        Console.Write($"Insira o nome [{i}, {j}]: ");
+        nomes[i, j] = Console.ReadLine();
+    }
+}
+
+Console.WriteLine("\nMOSTRANDO OS NOMES");
+for(int i = 0; i < nomes.GetLength(0); i++)
+{
+    for(int j = 0; j < nomes.GetLength(1); j++)
+    {
+        Console.Write($"[{i}, {j}] = {nomes[i, j]}\t");
+    }
+    Console.WriteLine();
+}
+
+Console.ReadKey();
