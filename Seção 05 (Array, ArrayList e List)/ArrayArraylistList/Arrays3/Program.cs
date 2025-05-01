9968798d417a32d832499d6efe53370bad0fb81e
IcaroRaphael <icaro2551@gmail.com>
2025-05-01 15:14:08 -0300

    Seção 05 (Arrays III)

---
+++ Seção 05 (Array, ArrayList e List)/ArrayArraylistList/Arrays3/Program.cs
@@ -0,0 +1,21 @@
+﻿//Declaração do array
+int[] numeros;
+
+//Alocação de memória
+numeros = new int[10] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+string[] nomes = {"Ana", "Maria", "Marta", "Paulo", "Carlos" };
+
+Console.WriteLine();
+foreach(int numero in numeros)
+{
+    Console.WriteLine(numero);
+}
+
+Console.WriteLine();
+foreach(string nome in nomes)
+{
+    Console.WriteLine(nome);
+}
+
+Console.ReadKey();
