98a57c77476789e90a18e49fc8c4699c16938375
IcaroRaphael <icaro2551@gmail.com>
2025-05-25 16:12:31 -0300

    Seção 05 (ArrayList IV)

---
+++ Seção 05 (Array, ArrayList e List)/ArrayArraylistList/ArrayList4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+
+// var lista = new ArrayList() { "Maria", 5, true, 4.5, null, DateTime.Now };
+// //Verificar se um elemento existe na coleção
+// var res1 = lista.Contains(5); //true
+// bool res2 = lista.Contains("Mari"); //false
+// Console.WriteLine(res1);
+// Console.WriteLine(res2);
+// Console.WriteLine(lista.Contains(null)); //true
+
+var lista = new ArrayList() { "Maria", "Ana", "Zilda", "Carlos", "Diná" };
+lista.Sort();
+foreach (var item in lista)
+{
+    System.Console.WriteLine(item);
+}
+
+lista.Clear();
+System.Console.WriteLine("Número de elementos do Array depois do clear: " + lista.Count);
+
+Console.ReadKey();
