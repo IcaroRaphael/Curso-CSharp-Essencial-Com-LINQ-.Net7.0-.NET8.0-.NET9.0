3af2b99cbaf620a1b788f98d03046f0f94856387
IcaroRaphael <icaro2551@gmail.com>
2025-07-02 10:07:35 -0300

    Seção 05 (List - Métodos Find)

---
+++ Seção 05 (Array, ArrayList e List)/ArrayArraylistList/MetodosFind/Program.cs
@@ -0,0 +1,19 @@
+﻿List<string> frutas = new()
+{
+    "Uva", "Banana", "Pera", "Maça", "Abacate", "Laranja", "Morango"
+};
+
+//Usando Predicado como uma função
+var fruta1 = frutas.Find(Procura);
+Console.WriteLine($"Predicado => {fruta1}");
+
+//Usando a expressão lambda
+var fruta2 = frutas.Find(i => i.Contains('n'));
+Console.WriteLine($"Expressão lambda => {fruta2}");
+
+Console.ReadKey();
+
+static bool Procura(string item)
+{
+    return item.Contains('n');
+}
